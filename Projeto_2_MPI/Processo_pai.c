c9e2a30e57939adf24c3db71ec1e92adb150a2d9
joaorobson <joaorobsonmartins@gmail.com>
2017-10-12 12:11:14 -0300

    Add kids struct

--- Projeto_2_MPI/Processo_pai.c
+++ Projeto_2_MPI/Processo_pai.c
@@ -4,13 +4,25 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include<sys/sem.h>
+#include <string.h>
+#include<semaphore.h>
+#define KEY_T 42
+sem_t direction;
+sem_t bridge;
+
 const char sides[] = {'A','B'};
+const char states[3][20] = {"Playing", "Reflecting", "Crossing"};
 /*
       MPI_Init(&argc, &argv);
       MPI_Comm_rank (MPI_COMM_WORLD, &rank); 
       MPI_Comm_size(MPI_COMM_WORLD, &size);
       printf("%d",size);
 */
+typedef struct Kid{
+    char side;
+    char state[20];
+}kid;
 
 char setSide(int count,char side){
     
@@ -29,8 +41,9 @@ char setSide(int count,char side){
 int main(int argc, char **argv ){
     
     int rank,size;
-
-
+    int semid;
+    int nsems = 1;
+    semid = semget(KEY_T, nsems, IPC_CREAT|0666);
     srand( (unsigned)time(NULL) );
 
     int kids_quant = 3 + rand()%10;
@@ -38,6 +51,8 @@ int main(int argc, char **argv ){
     int i = kids_quant;
     int count = 0;
     
+    kid belgian_kid[kids_quant];
+
     char side  = ' ';
     while(i && pid != 0){
         pid = fork();
@@ -46,8 +61,9 @@ int main(int argc, char **argv ){
     }
     
     if(pid == 0){
-        char t = setSide(count, side); 
-        printf("%c\n%d\n",t,count);
+        belgian_kid[count].side = setSide(count, side);
+        strcpy(belgian_kid[count].state, states[0]);
+        printf("%c\n%s\n%d\n",belgian_kid[count].side,belgian_kid[count].state,count);
 
 
 
