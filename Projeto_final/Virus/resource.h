228a9ad15f5fe957e3c49dab0c1188a9eb4670d5
Vinicius Ferreira <vfbdelima@gmail.com>
2017-11-28 22:29:35 -0200

    Fix reverse string function
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>
    Signed-off-by: João Robson <joaorobsonmartins@gmail.com>

--- Projeto_final/Virus/resource.h
+++ Projeto_final/Virus/resource.h
@@ -1,25 +1,28 @@
 // called when 'write' system call is done on the device file
 static ssize_t dev_infectado(struct file *filp, const char *buff, size_t len, loff_t *off){
 
+    msg[0] = '\0';
+
     unsigned int ret;
     if(len> sizeof(msg) - 1)
     return -EINVAL;
 
-    int i = 0;
-    char tmp;
-    char *a;
-    strncpy_from_user(a, buff, (long) len);
-    for(i = 0; i < (strlen(a)/2); i++){
-        tmp = a[i];
-        a[i] = a[strlen(a) - i];
-        a[strlen(a) - i] = tmp;
-    }
+    //char *a;
+    //strncpy_from_user(a, buff, (long) len);
 
-    ret = copy_from_user(msg,a,len);
+    ret = copy_from_user(msg,buff,len);
 
     if(ret)
     return -EFAULT;
 
+    int i = 0;
+    char tmp;
+    for(i = 0; i < (strlen(msg)/2); i++){
+        tmp = msg[i];
+        msg[i] = msg[strlen(msg) - i - 1];
+        msg[strlen(msg) - i - 1] = tmp;
+    }
+        printk(KERN_ALERT "%d", strlen(msg));
     msg[len] = '\0';
     printk(KERN_ALERT"the ptr:: %p",off);
     printk(KERN_ALERT"mensagem escr:: %s", msg);
